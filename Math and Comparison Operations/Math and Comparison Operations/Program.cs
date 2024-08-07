348c9992a7e21774024cc324d03a9b88d4203623
Brady-Leger <159858263+Brady-Leger@users.noreply.github.com>
2024-08-07 16:53:21 -0600

    Console Assignment

---
+++ Math and Comparison Operations/Math and Comparison Operations/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_and_Comparison_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 17;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //double decimalDivide = 100.0 / 17.0;
+            //Console.WriteLine(decimalDivide);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+
+            ////bool isWarm = currentTemperature >= roomTemperature;
+            //bool isWarm = currentTemperature != roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            double multiplication = 7.0 * 50.0;
+            Console.WriteLine("Seven Multiplied by Fifty = " + multiplication.ToString());
+            Console.ReadLine();
+
+            int add = 7 + 25;
+            Console.WriteLine("Seven Added to Twenty-Five = " + add.ToString());
+            Console.ReadLine();
+
+            double divide = 7.0 / 12.5;
+            Console.WriteLine("Seven Divided by Fourteen = " + divide.ToString());
+            Console.ReadLine();
+
+            bool greater = 7 > 50;
+            Console.WriteLine("Is Seven Greater Than Fifty? " + greater.ToString());
+            Console.ReadLine();
+
+            int leftover = 50 % 7;
+            Console.WriteLine("What is the remainder of 50 / 7? " + leftover.ToString());
+            Console.ReadLine();
+        }
+    }
+}
