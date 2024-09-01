6f40fea3217084089e0c9543e1f3b90a6e8b72e2
Brady-Leger <159858263+Brady-Leger@users.noreply.github.com>
2024-09-01 15:28:50 -0600

    Method Class Assignment

---
+++ Method Class Assignment/Mathod Class Assignment/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathod_Class_Assignment
+{
+    class MathOperation
+    {
+        //This method should be taking two integers as parameters. It will then do a math operation
+        //on the first integer and display the second integer to the screen
+        public void MathOp(int first, int second)
+        {
+            int result = first + 5;
+            Console.WriteLine(second);
+        }
+    }
+}
---
+++ Method Class Assignment/Mathod Class Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathod_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Here we will Instantiate the class
+            MathOperation foo = new MathOperation();
+            //User Input asking for an integer
+            Console.WriteLine("Enter an integer");
+            int i = Convert.ToInt32(Console.ReadLine());
+            //Asking the user for a second integer, this one will print to screen
+            Console.WriteLine("Enter a second integer");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The second integer you provided was " + j);
+            
+
+            foo.MathOp(i, j);
+            foo.MathOp(first: i, second: j);
+           
+            Console.ReadLine();
+        }
+    }
+}
